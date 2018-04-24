051bc95a83a16e244801e1c66cf16d8519a2d9fb
ShawnDH <37811310+ShawnDH@users.noreply.github.com>
2018-04-24 13:25:58 -0500

    update Multi Level Guessing Game

--- Multi Level Guessing Game/Multi Level Guessing Game/Source.cpp
+++ Multi Level Guessing Game/Multi Level Guessing Game/Source.cpp
@@ -54,23 +54,30 @@ int level2(int& count)
 	count = 1;
 	//initialize random seed
 	srand(time(NULL));
-	//generate secret number between 1 and 100
-	secret = rand() % 100 + 1;
-	do{
-		cout << "Please pick a number between 1-100 \n >";
-		cin >> numChoice;
-
-		if (numChoice > secret)
-		{
-			cout << "The number you picked is too high, guess again." << endl;
-			count = count + 1;
-		}
-		else if (numChoice < secret)
-		{
-			cout << "The number you picked is too low, guess again." << endl;
-			count = count + 1;
-		}
-	} while (numChoice != secret);
+	for (int c = 0; c <= 5; c++)
+	{
+		//generate secret number between 1 and 100
+		secret = rand() % 100 + 1;
+		cout << "Please pick a number between 1-100" << endl;
+		do{
+			cout << ">";
+			cin >> numChoice;
+			if (numChoice > secret)
+			{
+				cout << "The number you picked is too high, guess again." << endl;
+				count = count + 1;
+			}
+			else if (numChoice < secret)
+			{
+				cout << "The number you picked is too low, guess again." << endl;
+				count = count + 1;
+			}
+			else if (numChoice == secret)
+			{
+				cout << "Correct! " << secret << " is the correct number!" << endl;
+			}
+		} while (numChoice != secret);
+	}
 	r = count;
 	return  r;
 }
@@ -83,23 +90,30 @@ int level3(int& count)
 	count = 1;
 	//initialize random seed
 	srand(time(NULL));
-	//generate secret number between 1 and 1000
-	secret = rand() % 1000 + 1;
-	do{
-		cout << "Please pick a number between 1-1000 \n >";
-		cin >> numChoice;
-
-		if (numChoice > secret)
-		{
-			cout << "The number you picked is too high, guess again." << endl;
-			count = count + 1;
-		}
-		else if (numChoice < secret)
-		{
-			cout << "The number you picked is too low, guess again." << endl;
-			count = count + 1;
-		}
-	} while (numChoice != secret);
+	for (int c = 0; c <= 5; c++)
+	{
+		//generate secret number between 1 and 1000
+		secret = rand() % 1000 + 1;
+		cout << "Please pick a number between 1-1000" << endl;
+		do{
+			cout << ">";
+			cin >> numChoice;
+			if (numChoice > secret)
+			{
+				cout << "The number you picked is too high, guess again." << endl;
+				count = count + 1;
+			}
+			else if (numChoice < secret)
+			{
+				cout << "The number you picked is too low, guess again." << endl;
+				count = count + 1;
+			}
+			else if (numChoice == secret)
+			{
+				cout << "Correct! " << secret << " is the correct number!" << endl;
+			}
+		} while (numChoice != secret);
+	}
 	r = count;
 	return  r;
 }
